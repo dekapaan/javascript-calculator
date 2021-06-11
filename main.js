e045e10efd2cb06bb351ec47467a1bf9ac7e0402
dekapaan <terblancheronald@gmail.com>
2021-06-11 16:23:42 +0200

    finished up

--- main.js
+++ main.js
@@ -9,28 +9,106 @@ let btnEqual = document.getElementById("equal");
 
 let currentScreen = document.getElementById("currentNum");
 let previousScreen = document.getElementById("previousNum");
-let previousNum = "";
-let currentNum = "";
+let secondNum = "";
+let firstNum = "";
+
+let operand = "";
+
+let equalPressed = false;
 
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", append);
 }
 
-period.addEventListener("click", append);
-btnClear.addEventListener("click", clear);
-
-function append(e) {
-  if (e.target.innerHTML == "." && currentNum.includes(".")) {
-    currentNum;
-  } else if (currentNum == "" && e.target.innerHTML == ".") {
-    currentNum += "0" + e.target.innerHTML;
-  } else {
-    currentNum += e.target.innerHTML;
-  }
-  currentScreen.innerHTML = currentNum;
+for (let i = 0; i < arithmetic.length; i++) {
+  arithmetic[i].addEventListener("click", changescreen);
 }
 
-function clear(e) {
-  currentNum = "";
-  currentScreen.innerHTML = currentNum;
+period.addEventListener("click", append);
+btnClear.addEventListener("click", clear);
+btnEqual.addEventListener("click", operation);
+
+function append(e) {
+  if (equalPressed == true) {
+    equalPressed = false;
+    currentScreen.innerHTML = "";
+    firstNum = "";
+    firstNum += e.target.innerHTML;
+    currentScreen.innerHTML += e.target.innerHTML;
+    console.log(currentScreen.innerHTML);
+  } else if (currentScreen.innerHTML == "0" && e.target.innerHTML == "0") {
+    firstNum;
+    currentScreen;
+  } else if (currentScreen.innerHTML == "0" && e.target.innerHTML !== ".") {
+    firstNum;
+    currentScreen;
+  } else if (e.target.innerHTML == ".") {
+    if (currentScreen.innerHTML == "") {
+      firstNum = "0" + e.target.innerHTML;
+      currentScreen.innerHTML += "0" + e.target.innerHTML;
+    } else if (firstNum.includes(".")) {
+      firstNum;
+      currentScreen;
+    } else {
+      firstNum += e.target.innerHTML;
+      currentScreen.innerHTML += e.target.innerHTML;
+    }
+  } else {
+    firstNum += e.target.innerHTML;
+    currentScreen.innerHTML += e.target.innerHTML;
+  }
+}
+
+function clear() {
+  firstNum = "";
+  previousNum = "";
+  currentScreen.innerHTML = "";
+  previousScreen.innerHTML = "";
+  equalPressed = false;
+  operand = "";
+}
+
+function changescreen(e) {
+  if (operand !== "") {
+    previousScreen.innerHTML = operation() + e.target.innerHTML;
+    currentScreen.innerHTML = "";
+    firstNum = "";
+  } else {
+    operand = e.target.innerHTML;
+    secondNum = firstNum;
+    firstNum = "";
+    previousScreen.innerHTML = currentScreen.innerHTML + e.target.innerHTML;
+    currentScreen.innerHTML = "";
+  }
+}
+
+function operation(e) {
+  firstNum = currentScreen.innerHTML;
+  equalPressed = true;
+  if (currentScreen.innerHTML == "") {
+    currentScreen.innerHTML = 0;
+  } else if (operand == "+") {
+    answer = parseInt(secondNum) + parseInt(firstNum);
+    currentScreen.innerHTML - "";
+    currentScreen.innerHTML = answer;
+    operand = "";
+  } else if (operand == "-") {
+    answer = parseInt(secondNum) - parseInt(firstNum);
+    currentScreen.innerHTML - "";
+    currentScreen.innerHTML = answer;
+    operand = "";
+  } else if (operand == "/") {
+    answer = parseInt(secondNum) / parseInt(firstNum);
+    currentScreen.innerHTML - "";
+    currentScreen.innerHTML = answer;
+    operand = "";
+  } else if (operand == "x") {
+    answer = parseInt(secondNum) * parseInt(firstNum);
+    currentScreen.innerHTML = "";
+    currentScreen.innerHTML = answer;
+    operand = "";
+  }
+  firstNum = currentScreen.innerHTML;
+  previousScreen.innerHTML = "";
+  return answer;
 }
